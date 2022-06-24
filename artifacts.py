44740e4d1027507f4b9d6bae513c32a50c38182d
Hyeokjune Jeon <hyeokjune.jeon@furiosa.ai>
2022-06-24 11:35:39 +0900

    pytest 모듈 추가

--- artifacts.py
+++ artifacts.py
@@ -1,6 +1,6 @@
 import os
 from typing import Any
-
+import logging
 import aiohttp
 import dvc.api
 from furiosa.registry import Format, Metadata, Model, Publication
@@ -11,12 +11,16 @@ from furiosa.artifacts.vision.models.object_detection import (
     MLCommonsSSDSmallModel,
 )
 
+module_logger = logging.getLogger(__name__)
 
 async def load_dvc(uri: str):
+    dvc_repo = os.environ.get("DVC_REPO", None)
+    dvc_rev = os.environ.get("DVC_REV", None) 
+    module_logger.debug(f"dvc_uri={uri}, DVC_REPO={dvc_repo}, DVC_REV={dvc_rev}")
     async with aiohttp.ClientSession() as session:
         async with session.get(
             dvc.api.get_url(
-                uri, repo=os.environ.get("DVC_REPO", None), rev=os.environ.get("DVC_REV", None)
+                uri, repo=dvc_repo, rev= dvc_rev
             )
         ) as resp:
             resp.raise_for_status()
